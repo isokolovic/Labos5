a0c0ebe37a8bdefd55b9930ef48221be49c095c2
Ivan Sokolović <isokolovic1@gmail.com>
2021-12-16 19:06:31 +0100

    Add project files.

---
+++ labos5/Login.ascx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+namespace labos5
+{
+    public partial class Login : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private bool CheckUserNamePassword()
+        {
+            XElement korisnici = XElement.Load("./App_Data/korisnici.xml");
+
+            var users = from user in korisnici.Elements("korisnik")
+                        select new
+                        {
+                            username = (string)user.Element("korisnickoIme"),
+                            password = (string)user.Element("lozinka")
+                        };
+
+            foreach (var user in users)
+            {
+                if (string.Compare(user.username, TextBoxUsername.Text, true) == 0 & user.password == TextBoxPassword.Text)
+                {
+                    return true;
+                }
+
+            }
+            return false;
+        }
+
+
+        protected void ButtonLogin_Click(object sender, EventArgs e)
+        {
+            if (!CheckUserNamePassword())
+            {
+                PanelError.Visible = true;
+            }
+            else
+            {
+                DisplayBooks();
+            }
+        }
+
+        private void DisplayBooks()
+        {
+            PanelDisplay.Visible = true;
+
+            using(DataSet ds = new DataSet())
+            {
+                ds.ReadXml("./App_Data/popisKnjiga.xml");
+                GridViewData.DataSource = ds;
+                GridViewData.DataBind(); 
+            }
+        }
+    }
+}
---
+++ labos5/WebFormMain.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebFormMain.aspx.cs" Inherits="labos5.WebFormMain" %>
+
+<%@ Register Src="~/Login.ascx" TagPrefix="uc1" TagName="Login" %>
+
+
+<!DOCTY<uc1:Login runat="server" id="Login" />PE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <uc1:Login ID="Login1" runat="server" />
+    </form>
+</body>
+</html>
